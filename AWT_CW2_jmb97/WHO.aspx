7f16d55e4b2f5020e36e50961e6f5f5752f0c25d
Jay Bharakhada <bharakhdajay@gmail.com>
2020-12-04 00:52:20 +0000

    complete

---
+++ AWT_CW2_jmb97/WHO.aspx
@@ -0,0 +1,140 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="WHO.aspx.cs" Inherits="AWT_CW2_jmb97.WHO" %>
+
+<%@ Register Assembly="System.Web.DataVisualization, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" Namespace="System.Web.UI.DataVisualization.Charting" TagPrefix="asp" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+     <div class="container-fluid">
+        <div class="row">
+             <div class="col mx-auto">
+                <div class="card">
+                    <div class="card-body">
+                         <div class="row">
+                          <div class="col-md-9">
+                                <label>Country Code</label>
+                               <div class="form-group">
+                                <div class="input-group">
+                                     <asp:Button CssClass="btn btn-success" ID="btLoad" runat="server" Text="Load" OnClick="btLoad_Click"></asp:Button>
+                                    <asp:DropDownList CssClass="form-control" ID="ddlCountryCode" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlCountryCode_SelectedIndexChanged" Visible="false">
+                                        <asp:ListItem Text="Select" Value=""/> 
+                                    </asp:DropDownList>                             
+                                </div>
+                               </div>
+                            </div>
+
+                            <div class="col-md-3">
+                                <asp:Label runat="server" Visible="false" ID="Label1">Total Countries Loaded</asp:Label>
+                                <div class="form-group">
+                                   <asp:Label CssClass="form-control" runat="server" ID="lbTotalCountries" Visible="false"></asp:Label>
+   
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-1">
+                                <asp:Label runat="server">New Cases</asp:Label>
+                            </div>
+                            <div class="col-md-10">
+                               <div class="form-group">
+                                <div class="input-group">
+                                    <asp:Chart ID="Chart1" CssClass="align-content-md-center" runat="server" BackGradientStyle="LeftRight" Height="150px" Palette="None"   
+                                        PaletteCustomColors="192, 0, 0" Width="1300px">  
+                                        <Series>  
+                                            <asp:Series Name="Series1">  
+                                            </asp:Series>  
+                                        </Series>  
+                                        <ChartAreas>  
+                                            <asp:ChartArea Name="ChartArea1" >  
+                                            </asp:ChartArea>  
+                                        </ChartAreas>  
+                                        <BorderSkin BackColor="" PageColor="" />  
+                                    </asp:Chart>  
+                                </div>
+                               </div>
+                            </div>
+                        </div>  
+                        
+                        <div class="row">
+
+                             <div class="col-md-1">
+                                <asp:Label runat="server">Cumulative Cases</asp:Label>
+                            </div>
+                            <div class="col-md-10">
+                               <div class="form-group">
+                                <div class="input-group">
+                                    <asp:Chart ID="Chart2" runat="server" BackColor=""   
+                                        BackGradientStyle="LeftRight" Height="150px" Palette="None"   
+                                        PaletteCustomColors="192, 0, 0" Width="1300px">  
+                                        <Series>  
+                                            <asp:Series Name="Series1">  
+                                            </asp:Series>  
+                                        </Series>  
+                                        <ChartAreas>  
+                                            <asp:ChartArea Name="ChartArea1" >  
+                                            </asp:ChartArea>  
+                                        </ChartAreas>  
+                                        <BorderSkin BackColor="" PageColor="" />  
+                                    </asp:Chart>  
+                                </div>
+                               </div>
+                            </div>
+                        </div> 
+                        
+                        <div class="row">
+                             <div class="col-md-1">
+                                <asp:Label runat="server">New Deaths</asp:Label>
+                            </div>
+                            <div class="col-md-10">
+                               <div class="form-group">
+                                <div class="input-group">
+                                    <asp:Chart ID="Chart3" runat="server" BackColor=""   
+                                        BackGradientStyle="LeftRight" Height="150px" Palette="None"   
+                                        PaletteCustomColors="192, 0, 0" Width="1300px">  
+                                        <Series>  
+                                            <asp:Series Name="Series1">  
+                                            </asp:Series>  
+                                        </Series>  
+                                        <ChartAreas>  
+                                            <asp:ChartArea Name="ChartArea1" >  
+                                            </asp:ChartArea>  
+                                        </ChartAreas>  
+                                        <BorderSkin BackColor="" PageColor="" />  
+                                    </asp:Chart>   
+                                </div>
+                               </div>
+                            </div>
+                        </div>     
+                        
+
+                          <div class="row">
+                             <div class="col-md-1">
+                                <asp:Label runat="server">Cumulative Deaths</asp:Label>
+                            </div>
+                            <div class="col-md-10">
+                               <div class="form-group">
+                                <div class="input-group">
+                                  <asp:Chart ID="Chart4" runat="server" BackColor=""   
+                                        BackGradientStyle="LeftRight" Height="150px" Palette="None"   
+                                        PaletteCustomColors="192, 0, 0" Width="1300px">  
+                                        <Series>  
+                                            <asp:Series Name="Series1">  
+                                            </asp:Series>  
+                                        </Series>  
+                                        <ChartAreas>  
+                                            <asp:ChartArea Name="ChartArea1" >  
+                                            </asp:ChartArea>  
+                                        </ChartAreas>  
+                                        <BorderSkin BackColor="" PageColor="" />  
+                                    </asp:Chart>  
+                                </div>
+                               </div>
+                            </div>
+                        </div> 
+
+                    </div>
+                 </div>
+              </div>
+          </div>
+      </div>
+</asp:Content>
---
+++ AWT_CW2_jmb97/WHO.aspx.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Web.Configuration;
+using System.Text;
+using System.Web.UI.DataVisualization.Charting;
+
+
+namespace AWT_CW2_jmb97
+{
+    public partial class WHO : System.Web.UI.Page
+    {
+        string constr = WebConfigurationManager.ConnectionStrings["whodataConnectionString"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Bindchart();
+            Bindchart1();
+            Bindchart2();
+            Bindchart3();
+        }
+
+        private void Bindchart()
+        {
+            using (MySqlConnection con = new MySqlConnection(constr))
+            {
+                try
+                {
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+                    MySqlCommand cmd = new MySqlCommand("SELECT Date_reported, New_cases FROM whodataset where Country_code='" + ddlCountryCode.SelectedValue + "';", con);
+                    MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                    DataSet ds = new DataSet();
+                    da.Fill(ds);
+
+                    DataTable ChartData = ds.Tables[0];
+
+                    //storing total rows count to loop on each Record  
+                    string[] XPointMember = new string[ChartData.Rows.Count];
+                    int[] YPointMember = new int[ChartData.Rows.Count];
+
+                    for (int count = 0; count < ChartData.Rows.Count; count++)
+                    {
+                        //storing Values for X axis  
+                        XPointMember[count] = ChartData.Rows[count]["Date_reported"].ToString();
+                        //storing values for Y Axis  
+                        YPointMember[count] = Convert.ToInt32(ChartData.Rows[count]["New_cases"]);
+
+
+                    }
+                    //binding chart control  
+                    Chart1.Series[0].Points.DataBindXY(XPointMember, YPointMember);
+
+                    //Setting width of line  
+                    Chart1.Series[0].BorderWidth = 1;
+                    //setting Chart type   
+                    Chart1.Series[0].ChartType = SeriesChartType.Line;
+                    Chart1.ChartAreas["ChartArea1"].AxisX.MajorGrid.Enabled = false;
+                    Chart1.ChartAreas["ChartArea1"].AxisY.MajorGrid.Enabled = false;
+                    Chart1.Series[0].ChartType = SeriesChartType.Spline;
+                    Chart1.ChartAreas["ChartArea1"].Area3DStyle.Enable3D = true;
+
+
+                    con.Close();
+
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("<script>alert('" + ex.Message + "')</script");
+                }
+            }
+        }
+
+        private void Bindchart1()
+        {
+            using (MySqlConnection con = new MySqlConnection(constr))
+            {
+                try
+                {
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+                    MySqlCommand cmd = new MySqlCommand("SELECT Date_reported, Cumulative_cases FROM whodataset where Country_code='" + ddlCountryCode.SelectedValue + "';", con);
+                    MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                    DataSet ds = new DataSet();
+                    da.Fill(ds);
+
+                    DataTable ChartData = ds.Tables[0];
+
+                    //storing total rows count to loop on each Record  
+                    string[] XPointMember = new string[ChartData.Rows.Count];
+                    int[] YPointMember = new int[ChartData.Rows.Count];
+
+                    for (int count = 0; count < ChartData.Rows.Count; count++)
+                    {
+                        //storing Values for X axis  
+                        XPointMember[count] = ChartData.Rows[count]["Date_reported"].ToString();
+                        //storing values for Y Axis  
+                        YPointMember[count] = Convert.ToInt32(ChartData.Rows[count]["Cumulative_cases"]);
+
+
+                    }
+                    //binding chart control  
+                    Chart2.Series[0].Points.DataBindXY(XPointMember, YPointMember);
+
+                    //Setting width of line  
+                    Chart2.Series[0].BorderWidth = 1;
+                    //setting Chart type   
+                    Chart2.Series[0].ChartType = SeriesChartType.Line;
+                    Chart2.ChartAreas["ChartArea1"].AxisX.MajorGrid.Enabled = false;
+                    Chart2.ChartAreas["ChartArea1"].AxisY.MajorGrid.Enabled = false;
+                    Chart2.Series[0].ChartType = SeriesChartType.Spline;
+                    Chart2.ChartAreas["ChartArea1"].Area3DStyle.Enable3D = true;
+
+
+                    con.Close();
+
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("<script>alert('" + ex.Message + "')</script");
+                }
+            }
+        }
+
+        private void Bindchart2()
+        {
+            using (MySqlConnection con = new MySqlConnection(constr))
+            {
+                try
+                {
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+                    MySqlCommand cmd = new MySqlCommand("SELECT Date_reported, New_deaths FROM whodataset where Country_code='" + ddlCountryCode.SelectedValue + "';", con);
+                    MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                    DataSet ds = new DataSet();
+                    da.Fill(ds);
+
+                    DataTable ChartData = ds.Tables[0];
+
+                    //storing total rows count to loop on each Record  
+                    string[] XPointMember = new string[ChartData.Rows.Count];
+                    int[] YPointMember = new int[ChartData.Rows.Count];
+
+                    for (int count = 0; count < ChartData.Rows.Count; count++)
+                    {
+                        //storing Values for X axis  
+                        XPointMember[count] = ChartData.Rows[count]["Date_reported"].ToString();
+                        //storing values for Y Axis  
+                        YPointMember[count] = Convert.ToInt32(ChartData.Rows[count]["New_deaths"]);
+
+
+                    }
+                    //binding chart control  
+                    Chart3.Series[0].Points.DataBindXY(XPointMember, YPointMember);
+
+                    //Setting width of line  
+                    Chart3.Series[0].BorderWidth = 1;
+                    //setting Chart type   
+                    Chart3.Series[0].ChartType = SeriesChartType.Line;
+                    Chart3.ChartAreas["ChartArea1"].AxisX.MajorGrid.Enabled = false;
+                    Chart3.ChartAreas["ChartArea1"].AxisY.MajorGrid.Enabled = false;
+                    Chart3.Series[0].ChartType = SeriesChartType.Spline;
+                    Chart3.ChartAreas["ChartArea1"].Area3DStyle.Enable3D = true;
+
+
+                    con.Close();
+
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("<script>alert('" + ex.Message + "')</script");
+                }
+            }
+        }
+
+        private void Bindchart3()
+        {
+            using (MySqlConnection con = new MySqlConnection(constr))
+            {
+                try
+                {
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+                    MySqlCommand cmd = new MySqlCommand("SELECT Date_reported, Cumulative_deaths FROM whodataset where Country_code='" + ddlCountryCode.SelectedValue + "';", con);
+                    MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                    DataSet ds = new DataSet();
+                    da.Fill(ds);
+
+                    DataTable ChartData = ds.Tables[0];
+
+                    //storing total rows count to loop on each Record  
+                    string[] XPointMember = new string[ChartData.Rows.Count];
+                    int[] YPointMember = new int[ChartData.Rows.Count];
+
+                    for (int count = 0; count < ChartData.Rows.Count; count++)
+                    {
+                        //storing Values for X axis  
+                        XPointMember[count] = ChartData.Rows[count]["Date_reported"].ToString();
+                        //storing values for Y Axis  
+                        YPointMember[count] = Convert.ToInt32(ChartData.Rows[count]["Cumulative_deaths"]);
+
+
+                    }
+                    //binding chart control  
+                    Chart4.Series[0].Points.DataBindXY(XPointMember, YPointMember);
+
+                    //Setting width of line  
+                    Chart4.Series[0].BorderWidth = 1;
+                    //setting Chart type   
+                    Chart4.Series[0].ChartType = SeriesChartType.Line;
+                    Chart4.ChartAreas["ChartArea1"].AxisX.MajorGrid.Enabled = false;
+                    Chart4.ChartAreas["ChartArea1"].AxisY.MajorGrid.Enabled = false;
+                    Chart4.Series[0].ChartType = SeriesChartType.Spline;
+                    Chart4.ChartAreas["ChartArea1"].Area3DStyle.Enable3D = true;
+
+
+                    con.Close();
+
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("<script>alert('" + ex.Message + "')</script");
+                }
+            }
+        }
+
+        protected void btLoad_Click(object sender, EventArgs e)
+        {
+            ddlCountryCode.Visible = true;
+            Label1.Visible = true;
+            lbTotalCountries.Visible = true;
+            using (MySqlConnection con = new MySqlConnection(constr))
+            {
+                try
+                {
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+                    MySqlCommand cmd = new MySqlCommand("SELECT Country_code FROM whodataset GROUP BY Country_Code;", con);
+                    MySqlDataAdapter msda = new MySqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    msda.Fill(dt);
+                    var reader = cmd.ExecuteReader();
+                    if (dt.Rows.Count > 0)
+                    {
+                        while (reader.Read())
+                        {
+                            lbTotalCountries.Text = dt.Rows.Count.ToString() + " Countries Loaded";
+                            ddlCountryCode.Items.Add(reader.GetString("Country_code"));
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("<script>alert('" + ex.Message + "')</script");
+                }
+            }
+        }
+
+        protected void ddlCountryCode_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Bindchart();
+            Bindchart1();
+            Bindchart2();
+            Bindchart3();
+        }
+    }
+}
